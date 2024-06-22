46d93b0669cccb587b9b87eb27c8f7bbe6beb9c1
Roni <ronisank21@hotmail.com>
2024-06-21 23:00:23 -0300

    add button remove

--- index.js
+++ index.js
@@ -3,61 +3,87 @@
 const addList = document.getElementById('add-list');
 const includeInput = document.getElementById('include-input');
 const newsToday = document.getElementById('newsToday');
+const btnRemove = document.getElementsByClassName('btn-remove');
 
-let listaInteresses = [];
+let interestList = [];
 
 function include() {
     const addInput = document.getElementById('include-input').value;
-    listaInteresses.push(addInput);
-    localStorage.setItem('meusInteresses', JSON.stringify(listaInteresses));
+    interestList.push(addInput);
+    localStorage.setItem('meusInteresses', JSON.stringify(interestList));
+
     const addElement = document.createElement('li');
-    addElement.className = 'list-group-item list';
+    const addButton = document.createElement('button');
+
+    addButton.className = 'btn-remove';
     addElement.innerHTML += `<li>${addInput}</li>`;
+    addButton.innerHTML = `🗑️`;
+
+    addElement.appendChild(addButton);
     addList.appendChild(addElement);
+    
     includeInput.value = '';
-
+    removeItem();
 }
-
 function listClear() {
+    interestList = [];
     addList.innerHTML = '';
-    localStorage.removeItem('meusInteresses');
+    localStorage.clear();
+}
+function removeItem() {
+    const removeButton = document.getElementsByClassName('btn-remove');
+    for (let i = 0; i < removeButton.length; i++) {
+        removeButton[i].addEventListener('click', () => {
+            interestList.splice(i, 1);
+            localStorage.setItem('meusInteresses', JSON.stringify(interestList));
+            listLoad();
+        });
+    }
 }
 
 function listLoad() {
     addList.innerHTML = '';
+
     if (localStorage.getItem('meusInteresses')) {
-        listaInteresses = JSON.parse(localStorage.getItem('meusInteresses'));
-        console.log(listaInteresses);
-        listaInteresses.forEach((item) => {
+        interestList = JSON.parse(localStorage.getItem('meusInteresses'));
+
+        interestList.forEach((item) => {
             const addElement = document.createElement('li');
-            addElement.className = 'list-group-item list';
+            const addButton = document.createElement('button');
+
+            addElement.className = 'btn-remove';
             addElement.innerHTML = `<li>${item}</li>`;
+            addButton.innerHTML = `🗑️`;
+
+            addElement.appendChild(addButton);
             addList.appendChild(addElement);
         });
     } else {
         console.log('Não há itens salvos');
     }
+    removeItem();
 }
 const updateList = setInterval(listLoad, 1000);
-// listLoad();
 
 async function getListApi() {
-    const response = await fetch('https://servicodados.ibge.gov.br/api/v3/noticias/?tipo=release');
-    const data = await response.json();
+    try {
+        const response = await fetch('https://servicodados.ibge.gov.br/api/v3/noticias/?tipo=release');
+        const data = await response.json();
+        const newsTodayElement = document.getElementById('newsToday');
 
-    const newsTodayElement = document.getElementById('newsToday');
-    if (newsTodayElement) {
-        const indexAleatorio = Math.floor(Math.random() * data.items.length);
-        const NewsAleatoria = data.items[indexAleatorio];
+        if (newsTodayElement) {
+            const randomIndex = Math.floor(Math.random() * data.items.length);
+            const newsRandom = data.items[randomIndex];
+            newsTodayElement.innerHTML = `<p>${newsRandom.titulo}</p>`;
+        }
 
-        newsTodayElement.innerHTML = `<p>${NewsAleatoria.titulo}</p>`
-
+    } catch (error) {
+        console.error('Erro ao buscar notícias', error);
     }
 };
 function updateNews() {
     getListApi();
-    setTimeout(updateNews, 10000);
+    setTimeout(updateNews, 5000);
 };
-
-getListApi(); 
+getListApi();
 updateNews();
